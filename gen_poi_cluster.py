df43be7a7d42f684a5684866ac3fc2b294477991
yc <yc.1995>
2016-10-17 19:25:27 +0800

    16-10-17

--- gen_poi_cluster.py
+++ gen_poi_cluster.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import codecs
+import codecs,math
 
 def list_all_class():
 	dic = {}
@@ -42,6 +42,91 @@ def gen_mergeclass_label():
 		out.write('%d %d %f %s\n'%(i,count[i],float(count[i])/s,name[i]))
 	out.close()
 
+def select_ap():
+	dic = {}
+	fdic = {'1':1,'2':2,'7':3,'13':4,'16':5,'18':6,'21':7}
+	f = codecs.open('../data_beijing',encoding='UTF-8')
+	for line in f:
+		words = line[:-1].split('|')
+		bssid = words[4]
+		if dic.has_key(bssid):
+			dic[bssid]=dic[bssid]+1
+		else:
+			dic[bssid]=1
+	f.close()
+	out = codecs.open('../poi_selected','w',encoding='UTF-8')
+	f = codecs.open('../poi_merge',encoding='UTF-8')
+	for line in f:
+		words = line[:-1].split('|')
+		bssid = words[1]
+		fid = words[4]
+		if fdic.has_key(fid) and dic.has_key(bssid) and dic[bssid]>1:
+			out.write(line[:-1]+'|%d\n'%dic[bssid])
+	f.close()
+	out.close()
+
+def cluster_alg(dic, name):#dic[i]:[bssid, No., lon, lat, times, p, deta...]
+	fdic = {'1':'办公','2':'农业','7':'机场','13':'住宅','16':'大学','18':'公园','21':'餐厅'}
+	print 'processing ',fdic[name]
+	l = len(dic)
+	print 'N = %d'%l
+	d = [[0 for i in range(l)] for j in range(l)]
+	dc = 0.002
+	tc = 500
+	print 'Computing d...'
+	for i in range(l):
+		for j in range(l):
+			d[i][j]=math.sqrt((dic[i][2]-dic[j][2])**2+(dic[i][3]-dic[j][3])**2)
+	print 'computing p...'
+	for i in range(l):
+		p = 0
+		for j in range(l):
+			#if i!=j:
+			p=p+math.e**(-((d[i][j]/dc)**2)+math.sqrt(float(dic[j][4])/tc))
+		dic[i].append(p)
+	print 'sorting...'
+	dic = sorted(dic,key=lambda m:(-m[5]))
+	print 'computing deta...'
+	for i in range(l):
+		if i==0:
+			dic[0].append(0)
+			continue
+		deta=10000
+		for j in range(l):
+			if j>=i:
+				break
+			deta=min(deta,d[dic[i][1]][dic[j][1]])
+		dic[i].append(deta)
+		dic[0][6]=max(dic[0][6],deta)
+	out=codecs.open('result/p_deta_%s.txt'%name,'w',encoding='UTF-8')
+	for i in range(l):
+		out.write('%s|%f|%f|%d|%f|%f\n'%(dic[i][0],dic[i][2],dic[i][3],dic[i][4],dic[i][5],dic[i][6]))
+	out.close()
+
+def clustering():
+	dic = {'1':[],'2':[],'7':[],'13':[],'16':[],'18':[],'21':[]}
+	f = codecs.open('../poi_selected',encoding='UTF-8')
+	for line in f:
+		words = line[:-1].split('|')
+		bssid = words[1]
+		lon = words[2]
+		lat = words[3]
+		fid = words[4]
+		times = words[5]
+		l = len(dic[fid])
+		dic[fid].append([bssid,l,float(lon),float(lat),int(times)])
+	f.close()
+	for key in dic:
+		_input = dic[key]
+		if len(_input)>10000:
+			_input = sorted(_input,key=lambda m:-m[4])
+			_input = _input[:9999]
+			for i in range(9999):
+				_input[i][1]=i
+		cluster_alg(_input, key)
+
 if __name__ == '__main__':
 	#list_all_class()
-	gen_mergeclass_label()
+	#gen_mergeclass_label()
+	#select_ap()
+	clustering()
